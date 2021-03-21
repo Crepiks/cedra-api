244b94be84bbc56a70956b105005dcbc3b43f8ed
Crepiks <sayazhan.onlassyn@mail.ru>
2021-03-21 12:53:07 +0600

    fix: change the recommendation logic

--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,11 +1,23 @@
 import User from "../models/user.model";
 
+const MALE_GENDER_ID = 1;
+const FEMALE_GENDER_ID = 2;
+
 class FeedService {
   findUserByPhoneNumber = (phoneNumber: string) => User.query().findOne({ phoneNumber });
 
   getSuggestions = (user: User) => {
     const subquery = user.$relatedQuery("reactedByUser").select("recipientId");
-    return User.query().where("id", "not in", subquery).andWhereNot("id", "=", user.id);
+    if (user.genderId === 1 || user.genderId === 2) {
+      return User.query()
+        .where("id", "not in", subquery)
+        .andWhereNot("id", "=", user.id)
+        .andWhere({
+          genderId: user.genderId === MALE_GENDER_ID ? FEMALE_GENDER_ID : MALE_GENDER_ID,
+        });
+    } else {
+      return User.query().where("id", "not in", subquery).andWhereNot("id", "=", user.id);
+    }
   };
 }
 
